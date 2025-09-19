da08903669660a295d89dab6b45b0b75c2d1647e
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-19 20:41:40 -0300

    Feature erro, número negativo

--- lib/main.dart
+++ lib/main.dart
@@ -30,17 +30,29 @@ class _ContadorStatePage extends State<ContadorPage> {
     });
   }
 
-  void decrementar() {
-    setState(() {
-      contador--;
-    });
-  }
+  // void decrementar() {}
 
   void reset() {
     setState(() {
       contador = 0;
     });
   }
+
+  void decrementar(BuildContext context) {
+    if (contador <= 0) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('O número não pode ser negativo!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+    setState(() {
+      contador--;
+    });
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +65,8 @@ class _ContadorStatePage extends State<ContadorPage> {
           color: const Color.fromARGB(255, 19, 48, 97),
         ),
       ),
+
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -64,6 +78,8 @@ class _ContadorStatePage extends State<ContadorPage> {
                 color: const Color.fromARGB(255, 18, 28, 41),
               ),
             ),
+
+
             Text(
               "$contador",
               style: TextStyle(
@@ -71,6 +87,8 @@ class _ContadorStatePage extends State<ContadorPage> {
                 color: const Color.fromARGB(255, 43, 46, 53),
               ),
             ),
+
+
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -78,11 +96,15 @@ class _ContadorStatePage extends State<ContadorPage> {
                   onPressed: incrementar,
                   child: Text("Incrementar"),
                 ),
+
+
                 SizedBox(width: 20),
                 ElevatedButton(
-                  onPressed: decrementar,
+                  onPressed: () => decrementar(context),
                   child: Text("Decrementar"),
                 ),
+
+
                 SizedBox(width: 30),
                 ElevatedButton(onPressed: reset, child: Text("Resetar")),
               ],
